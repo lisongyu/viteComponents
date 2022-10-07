d796237017799c906bdd2da85c7aefadb1b6f3c7
lisongyu <lisongyu@bailian.ai>
2022-10-07 21:34:08 +0800

    提交处理单元测试完毕

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,18 @@ const rollupOptions = {
   };
 
 export default defineConfig({
- 
+  test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom',
+    // 支持tsx组件，很关键
+    transformMode: {
+      web: [/.[tj]sx$/]
+    }
+  },
+
     // 添加库模式配置
 
     build: {
