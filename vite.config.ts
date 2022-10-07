db1e90a5944f2d25b29d5d9a7320897715bcee02
lisongyu <lisongyu@bailian.ai>
2022-10-07 17:09:08 +0800

    提交样式处理完成

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+// import { presetUno, presetAttributify, presetIcons } from "unocss";
+// import Unocss from "unocss/vite";
+import Unocss from "./config/unocss";
+
+
 // https://vitejs.dev/config/
 
 const rollupOptions = {
 
     external: ["vue", "vue-router"],
+   
     output: {
+      assetFileNames: `assets/[name].css`,
       globals: {
         vue: "Vue",
       },
@@ -15,11 +22,15 @@ const rollupOptions = {
   };
 
 export default defineConfig({
+ 
     // 添加库模式配置
 
     build: {
         rollupOptions,
         minify:false,
+       
+        cssCodeSplit: true,   // 追加
+      
         lib: {
           entry: "./src/entry.ts",
           name: "SmartyUI",
@@ -32,7 +43,9 @@ export default defineConfig({
       // 添加JSX插件
       vueJsx({
         // options are passed on to @vue/babel-plugin-jsx
-      })
+      }),
+      // 添加UnoCSS插件
+      Unocss()
 ],
 
 });
