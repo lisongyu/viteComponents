be24f9f33e60b7c5b66cd46191fdda49467e5a45
lisongyu <lisongyu@bailian.ai>
2022-10-07 10:53:06 +0800

    提交基础处理

---
+++ src/entry.ts
@@ -0,0 +1,16 @@
+import { App } from "vue";
+import MyButton from "./button";
+import SFCButton from "./SFCButton.vue";
+import JSXButton from "./JSXButton";
+
+// 导出单独组件
+export { MyButton, SFCButton, JSXButton };
+
+
+export default {
+    install(app:App):void{
+        app.component(MyButton.name,MyButton);
+        app.component(SFCButton.name,SFCButton);
+        app.component(JSXButton.name,JSXButton);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// const s:string='Hello Typescript'
+
+// console.log(s)
+
+import { createApp } from "vue";
+
+// import SButton from "./button";
+// import SButton from "./button";
+
+import SFCButton from "./JSXButton";
+
+createApp(SFCButton)
+.mount("#app");
