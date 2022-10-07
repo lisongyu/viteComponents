8d52c355f7c8c258f46f50e2c34384d37959e7fc
lisongyu <lisongyu@bailian.ai>
2022-10-07 22:24:07 +0800

    提交修改

--- src/JSXButton.tsx
+++ src/JSXButton.tsx
@@ -1,10 +1,8 @@
-import { defineComponent, h } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-
   name: "JSXButton",
   render() {
     return <button>JSX Button</button>;
   },
-
 });
