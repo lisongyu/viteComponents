91d6c1ee8ea54e2aa32e8d006f45f3e9935ec3dc
lisongyu <lisongyu@bailian.ai>
2022-10-15 22:02:45 +0800

    feat: 文件名字修改

--- packages/quick-ui-vite/src/entry.ts
+++ packages/quick-ui-vite/src/entry.ts
@@ -1,5 +1,5 @@
 import { App } from "vue";
-import { Button } from "./Button/index";
+import { Button } from "./Button";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
 
